c7611aa7f56a4a5fbb91fd3fc94c2e176ef29ff0
Greg Marut <greg.marut@gmail.com>
2014-12-25 15:01:07 -0500

    check for null data for PUT/POST requests

--- src/main/java/com/gregmarut/resty/client/RestRequestFactory.java
+++ src/main/java/com/gregmarut/resty/client/RestRequestFactory.java
@@ -4,9 +4,8 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
  * Contributors:
- *    Greg Marut - initial API and implementation
+ * Greg Marut - initial API and implementation
  ******************************************************************************/
 package com.gregmarut.resty.client;
 
@@ -70,9 +69,13 @@ public class RestRequestFactory
 		HttpPost httpPost = new HttpPost(uri);
 		setHeaders(httpPost);
 		
-		ByteArrayEntity entity = new ByteArrayEntity(data);
-		entity.setContentType(contentType);
-		httpPost.setEntity(entity);
+		// make sure the data is not null
+		if (null != data)
+		{
+			ByteArrayEntity entity = new ByteArrayEntity(data);
+			entity.setContentType(contentType);
+			httpPost.setEntity(entity);
+		}
 		
 		return httpPost;
 	}
@@ -103,9 +106,13 @@ public class RestRequestFactory
 		HttpPut httpPut = new HttpPut(uri);
 		setHeaders(httpPut);
 		
-		ByteArrayEntity entity = new ByteArrayEntity(data);
-		entity.setContentType(contentType);
-		httpPut.setEntity(entity);
+		// make sure the data is not null
+		if (null != data)
+		{
+			ByteArrayEntity entity = new ByteArrayEntity(data);
+			entity.setContentType(contentType);
+			httpPut.setEntity(entity);
+		}
 		
 		return httpPut;
 	}
