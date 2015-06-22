c384d607c05de76026bf63c69abc8cd095f2b78d
Greg Marut <greg.marut@gmail.com>
2015-06-22 11:30:19 -0400

    allowed jsoninvocationhandler to accept a serializer

--- src/main/java/com/gregmarut/resty/client/JSONInvocationHandler.java
+++ src/main/java/com/gregmarut/resty/client/JSONInvocationHandler.java
@@ -1,12 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2014 Greg Marut.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- * 
- * Contributors:
- *    Greg Marut - initial API and implementation
+ * Copyright (c) 2014 Greg Marut. All rights reserved. This program and the accompanying materials are made available
+ * under the terms of the GNU Public License v3.0 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html Contributors: Greg Marut - initial API and implementation
  ******************************************************************************/
 package com.gregmarut.resty.client;
 
@@ -16,27 +11,34 @@ import com.gregmarut.resty.serialization.Serializer;
 public class JSONInvocationHandler extends RestInvocationHandler
 {
 	private final RestRequestFactory restRequestFactory;
-	private final Serializer serializer;
+	private final GsonSerializer serializer;
 	
 	public JSONInvocationHandler(final HttpClientFactory httpClientFactory, final String rootURL)
 	{
 		this(httpClientFactory, rootURL, new DefaultStatusCodeHandler());
 	}
 	
-	public JSONInvocationHandler(final HttpClientFactory httpClientFactory, final String rootURL, final StatusCodeHandler statusCodeHandler)
+	public JSONInvocationHandler(final HttpClientFactory httpClientFactory, final String rootURL,
+			final StatusCodeHandler statusCodeHandler)
+	{
+		this(httpClientFactory, rootURL, statusCodeHandler, new GsonSerializer());
+	}
+	
+	public JSONInvocationHandler(final HttpClientFactory httpClientFactory, final String rootURL,
+			final StatusCodeHandler statusCodeHandler, final GsonSerializer serializer)
 	{
 		super(httpClientFactory, rootURL, statusCodeHandler);
 		
 		restRequestFactory = new RestRequestFactory(RestRequestFactory.JSON_TYPE);
-		serializer = new GsonSerializer();
+		this.serializer = serializer;
 	}
-
+	
 	@Override
 	protected RestRequestFactory getRestRequestFactory()
 	{
 		return restRequestFactory;
 	}
-
+	
 	@Override
 	protected Serializer getSerializer()
 	{
