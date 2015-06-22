e9e0ed543bfb5ad2d7b932f0ba8115eee743cb29
Greg Marut <greg.marut@gmail.com>
2015-06-22 11:24:08 -0400

    allow a gson object to be passed into the serializer

--- src/main/java/com/gregmarut/resty/serialization/GsonSerializer.java
+++ src/main/java/com/gregmarut/resty/serialization/GsonSerializer.java
@@ -1,12 +1,7 @@
 /*******************************************************************************
- * Copyright (c) 2014 Greg Marut.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- * 
- * Contributors:
- *    Greg Marut - initial API and implementation
+ * Copyright (c) 2014 Greg Marut. All rights reserved. This program and the accompanying materials are made available
+ * under the terms of the GNU Public License v3.0 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html Contributors: Greg Marut - initial API and implementation
  ******************************************************************************/
 package com.gregmarut.resty.serialization;
 
@@ -19,7 +14,12 @@ public class GsonSerializer implements Serializer
 	
 	public GsonSerializer()
 	{
-		gson = new Gson();
+		this(new Gson());
+	}
+	
+	public GsonSerializer(final Gson gson)
+	{
+		this.gson = gson;
 	}
 	
 	@Override
@@ -35,9 +35,14 @@ public class GsonSerializer implements Serializer
 		{
 			return gson.fromJson(new String(bytes), clazz);
 		}
-		catch(JsonSyntaxException e)
+		catch (JsonSyntaxException e)
 		{
 			throw new SerializationException(e);
 		}
+	}
+	
+	public Gson getGson()
+	{
+		return gson;
 	}
 }
