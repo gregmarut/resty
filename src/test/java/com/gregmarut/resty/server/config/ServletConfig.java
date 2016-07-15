7020beb568e7b889083a7c4c33a81afa8ad70228
Greg Marut <greg.marut@gmail.com>
2016-07-14 23:11:53 -0400

    fixed import

--- src/test/java/com/gregmarut/resty/server/config/ServletConfig.java
+++ src/test/java/com/gregmarut/resty/server/config/ServletConfig.java
@@ -3,7 +3,6 @@ package com.gregmarut.resty.server.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
