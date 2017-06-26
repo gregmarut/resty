49be9c60fd85c03f73d75b73cd04b4d1e0c2d382
Greg Marut <greg.marut@gmail.com>
2017-06-26 19:59:45 -0400

    added android tests

--- resty-android/src/main/java/com/gregmarut/resty/URLInvocationHandler.java
+++ resty-android/src/main/java/com/gregmarut/resty/URLInvocationHandler.java
@@ -4,6 +4,13 @@ import com.gregmarut.resty.exception.WebServiceException;
 import com.gregmarut.resty.http.request.RestRequest;
 import com.gregmarut.resty.http.response.RestResponse;
 import com.gregmarut.resty.serialization.GsonSerializer;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
 
 public class URLInvocationHandler extends JSONInvocationHandler
 {
@@ -26,6 +33,52 @@ public class URLInvocationHandler extends JSONInvocationHandler
 	@Override
 	protected RestResponse executeRequest(final RestRequest request) throws WebServiceException
 	{
-		return null;
+		try
+		{
+			//open a new url connection for this request
+			HttpURLConnection httpURLConnection = openUrlConnection(request);
+			byte[] data = IOUtils.toByteArray(httpURLConnection.getInputStream());
+			
+			//create a new rest response
+			return new RestResponse(httpURLConnection.getResponseCode(), data);
+		}
+		catch (IOException e)
+		{
+			throw new WebServiceException(e);
+		}
+	}
+	
+	/**
+	 * opens a new url connection
+	 *
+	 * @param request
+	 * @return
+	 * @throws IOException
+	 */
+	private HttpURLConnection openUrlConnection(final RestRequest request) throws IOException
+	{
+		//create a new url object
+		URL url = new URL(request.getUrl());
+		
+		//open a new url connection
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod(request.getMethodType().toString());
+		
+		//for each of the headers
+		for (Map.Entry<String, String> header : request.getHeaders().entrySet())
+		{
+			//apply the header to this url connection
+			connection.setRequestProperty(header.getKey(), header.getValue());
+		}
+		
+		//check to see if there is a data object to send
+		if (null != request.getData())
+		{
+			//write the data to the output stream
+			OutputStream os = connection.getOutputStream();
+			IOUtils.write(request.getData(), os);
+		}
+		
+		return connection;
 	}
 }
